61c544fd7cb953ebeba95012ec8cc00403777c91
1337vch <128287155+1337vch@users.noreply.github.com>
2026-03-01 05:17:03 +0100

    Update and rename gce-image-build.sh to image-build.sh

---
+++ image-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -e
+wget https://storage.googleapis.com/new_os_image/os/Zorin-OS-18-Core-64-bit-r3.iso https://storage.googleapis.com/new_os_image/os/gce-image-build.sh https://storage.googleapis.com/new_os_image/os/setup_gcp_credentials.sh
